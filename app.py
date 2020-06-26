46b8fa3cf4775de63d576c7e2f7f644cd703b819
RegunLim <brownsoul89@naver.cp>
2020-06-26 20:47:58 +0900

    20200626

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, jsonify
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/written-homework')
+def writtenhomework():
+    return render_template('writtenHW.html')
+
+@app.route('/dictation-homework')
+def dictationhomework():
+    return render_template('dictationHW.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
