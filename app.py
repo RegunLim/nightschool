17a590d43c7eaf1930eb7579638c57a1f982852c
RegunLim <brownsoul89@naver.cp>
2020-06-27 14:51:54 +0900

    200627

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template, request, jsonify
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
+
 app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+db = client.dbtest
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -18,6 +22,11 @@ def dictationhomework():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/lesson1')
+def question():
+    all_questions = list(db.lesson.find({},{'_id':0}))
+    return jsonify({'result': 'success', 'lesson':all_questions})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
